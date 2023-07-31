615d68c285849ec4148f49f7e88dae7bef7ff7c1
Dolity <79091852+Dolity@users.noreply.github.com>
2023-08-01 01:44:14 +0700

    Initial commit

---
+++ API/getAgency.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+
+const admin = require("firebase-admin");
+const credentials = require("C:/FireStoreAPI/keyFS.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
+
+const db = admin.firestore();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Read All
+const GetAgenciesAll = async (req, res) => {
+    try {
+        const usersRef = db.collection("getCurrency");
+        const response = await usersRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.send(responseArr);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+const DeleteDocument = async (req, res) => {
+    const { email } = req.params; // รับ docId ที่ต้องการลบมาจาก URL
+
+    try {
+        // ลบเอกสารใน Firestore โดยใช้ docId
+        const userRecord = await admin.auth().getUserByEmail(email);
+        await db.collection("usersPIN").doc(userRecord.uid).delete();
+        res.status(200).json({ message: 'ลบเอกสารเรียบร้อยแล้ว' });
+    } catch (error) {
+        console.error('เกิดข้อผิดพลาดในการลบเอกสาร:', error);
+        res.status(500).json({ error: 'เกิดข้อผิดพลาดบางอย่าง กรุณาลองใหม่อีกครั้งในภายหลัง' });
+    }
+};
+
+const DeleteUser = async (req, res) => {
+    const { email } = req.params;
+
+    try {
+        const userRecord = await admin.auth().getUserByEmail(email);
+        await admin.auth().deleteUser(userRecord.uid);
+        res.status(200).json({ message: 'ลบบัญชีผู้ใช้งานเรียบร้อยแล้ว' });
+    } catch (error) {
+        console.error('เกิดข้อผิดพลาดในการลบบัญชีผู้ใช้:', error);
+        res.status(500).json({ error: 'เกิดข้อผิดพลาดบางอย่าง กรุณาลองใหม่อีกครั้งในภายหลัง' });
+    }
+};
+
+const DeleteAll = async (req, res) => {
+    const { email } = req.params;
+
+    try {
+        const userRecord = await admin.auth().getUserByEmail(email);
+        // Del Authen
+        await admin.auth().deleteUser(userRecord.uid);
+
+        // Del Doc
+        await db.collection("usersPIN").doc(userRecord.uid).delete();
+
+        res.status(200).json({ message: 'ลบบัญชีผู้ใช้และลบเอกสารเรียบร้อยแล้ว' });
+    } catch (error) {
+        console.error('เกิดข้อผิดพลาดในลบบัญชีผู้ใช้และลบเอกสาร:', error);
+        res.status(500).json({ error: 'เกิดข้อผิดพลาดบางอย่าง กรุณาลองใหม่อีกครั้งในภายหลัง' });
+    }
+};
+
+module.exports = {
+
+    GetAgenciesAll,
+    DeleteDocument,
+    DeleteUser,
+    DeleteAll
+
+}
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+
+const port = 5200;
+
+const dbFirestore = require('./API/getAgency');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+//CRUD_FireStore
+app.get('/agency', dbFirestore.GetAgenciesAll);
+app.delete('/deleteDoc/:email', dbFirestore.DeleteDocument);
+app.delete('/deleteUser/:email', dbFirestore.DeleteUser);
+app.delete('/deleteAll/:email', dbFirestore.DeleteAll);
+
+
+app.get('/', (req, res) => {
+  res.send('This is my API running...')
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route')
+});
+
+app.listen(port, () => {  //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`);
+});
+
+module.exports = app;
